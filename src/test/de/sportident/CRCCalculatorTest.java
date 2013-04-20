a01955bdce389ac9fa6af13b72c10226ab20a306
Simon Denier <simon.denier@gmail.com>
2013-04-21 00:39:37 +0200

    Add CRCCalcultator with CC BY SPORTident 3.0 license

---
+++ src/test/de/sportident/CRCCalculatorTest.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2013 Simon Denier
+ */
+package test.de.sportident;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import de.sportident.CRCCalculator;
+
+/**
+ * @author Simon Denier
+ * @since Feb 16, 2013
+ *
+ */
+public class CRCCalculatorTest {
+
+	@Test
+	public void testSampleCrc() {
+		byte[] sample = new byte[] {
+				(byte) 0x53, (byte) 0x00, (byte) 0x05, (byte) 0x01,
+				(byte) 0x0F, (byte) 0xB5, (byte) 0x00, (byte) 0x00,
+				(byte) 0x1E, (byte) 0x08
+		};
+		int expected_crc = 0x2C12;
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+	
+	@Test
+	public void testStartupCrc() {
+		int expected_crc = 0x6D0A;
+		byte[] sample = new byte[] { (byte) 0xF0, 0x01, 0x4D };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));		
+	}
+
+	@Test
+	public void testAnswerStartupCrc() {
+		int expected_crc = 0x8B12;
+		byte[] sample = new byte[] { (byte) 0xF0, 0x03, 0x01, 0x01, 0x4D };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));		
+	}
+	
+	@Test
+	public void test2AnswerStartupCrc() {
+		int expected_crc = 0x0D11;
+		byte[] sample = new byte[] { (byte) 0xF0, 0x03, 0x00, 0x01, 0x4D };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+
+	@Test
+	public void testGetSystemValueCrc() {
+		int expected_crc = 0x0414;
+		byte[] sample = new byte[] { (byte) 0x83, 0x02, 0x74, 0x01 };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+
+	@Test
+	public void testReadSiCard5() {
+		int expected_crc = 0xB100;
+		byte[] sample = new byte[] { (byte) 0xB1, 0x00 };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+
+	@Test
+	public void testReadSiCard8B0() {
+		int expected_crc = 0xE209;
+		byte[] sample = new byte[] { (byte) 0xEF, 0x01, 0x00 };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+
+	@Test
+	public void testReadSiCard8B1() {
+		int expected_crc = 0xE309;
+		byte[] sample = new byte[] { (byte) 0xEF, 0x01, 0x01 };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+
+	@Test
+	public void testReadSiCard10B8() {
+		int expected_crc = 0xEA09;
+		byte[] sample = new byte[] { (byte) 0xEF, 0x01, 0x08 };
+		assertThat(CRCCalculator.crc(sample), equalTo(expected_crc));
+	}
+	
+}
